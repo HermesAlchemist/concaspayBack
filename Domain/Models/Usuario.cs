1b9a8745a983fd883eddb3d9f188a84d56400e5f
Enéas Marques <eneasmarques@outlook.com.br>
2024-03-18 04:18:00 -0300

    feat: Adiciona DataAnnotations no Model Usuario

--- Domain/Models/Usuario.cs
+++ Domain/Models/Usuario.cs
@@ -9,15 +9,19 @@ public class Usuario
     public int Id { get; set; }
 
     [Required]
+    [StringLength(100)]
     public string Nome { get; set; } = string.Empty;
 
     [Required]
+    [StringLength(100)]
     public string Email { get; set; } = string.Empty;
 
     [Required]
+    [StringLength(11, MinimumLength = 11)]
     public string CPF { get; set; } = string.Empty;
 
     [Required]
+    [StringLength(100, MinimumLength = 6)]
     public string Senha { get; set; } = string.Empty;
 
     [Required]
